48558271626a0469e5db3c5d75d1f178e7aab2ec
MatCast <gozyssj4@gmail.com>
2019-06-25 21:43:25 +0200

    Added multiple lines and average line. Structure rewritten.

--- chart.js
+++ chart.js
@@ -1,3 +1,226 @@
+// SVG properties
+function svgChart() {
+  this.svgWidth = 1200;
+  this.svgHeight = 750;
+  // Height of the bottom chart: bottom margin of the top chart
+  this.chartsDiff = 250;
+  //  Top chart margins and inner measures
+  this.marginLines = { top: 50, right: 50, bottom: this.chartsDiff, left: 50 };
+  this.widthLines = this.svgWidth - this.marginLines.left - this.marginLines.right;
+  this.heightLines = this.svgHeight - this.marginLines.top - this.marginLines.bottom;
+
+  // Bottom chart margins and inner measures
+  this.fromTop = this.svgHeight - this.chartsDiff + 50;
+  this.marginBars = { top: this.fromTop, right: 50, bottom: 50, left: 50 };
+  this.widthBars = this.svgWidth - this.marginBars.left - this.marginBars.right;
+  this.heightBars = this.svgHeight - this.marginBars.top - this.marginBars.bottom;
+
+
+  // Dimensions and aspect ratio of the SVG
+  this.svg = d3.select('body')
+  .append('svg')
+  .attr('id', 'main-svg')
+  .attr('width', '100%')
+  .attr('preserveAspectRatio', 'xMidYMid meet')
+  .attr('viewBox', '0 0 ' + this.svgWidth + ' ' + this.svgHeight),
+
+  this.drawCurves = () =>
+    d3.csv('./distances.csv').then((data) => {
+      this.names = [];
+      this.colors = ['HotPink', 'gold', 'DodgerBlue', 'BlueViolet'];
+      data.forEach((d) => {
+        for (var key in d) {
+          if (d.hasOwnProperty(key)) {
+            d[key] = +d[key];
+            if (key != 'distance' && ! this.names.includes(key)){
+              this.names.push(key);
+            }
+          }
+        }
+      });
+
+      // var datas = [data1, data2];
+      // var c = ['HotPink', 'gold'];
+      var maxPerName = [];
+      this.names.forEach(function (name) {
+        maxPerName.push(d3.max(data, function(d){
+          return d[name];})
+        );
+      });
+      this.maxY = d3.max(maxPerName);
+
+      // X Scale
+      this.xScaleLines = d3.scaleLinear()
+      .domain([-0.5, data.length - 0.5])
+      .range([0, this.widthLines]);
+
+      // Y Scale
+      this.yScaleLines = d3.scaleLinear()
+      .domain([0, this.maxY])
+      .range([this.heightLines, 0]);
+
+      namesAvg(data).forEach((d,i) => {
+        data[i].average = d;
+      });
+      this.names.push('average');
+      this.colors.push('orange');
+
+      // Curves Group and translation
+      var lines = d3.select('#main-svg')
+        .append('g')
+        .attr('id', 'svg-lines')
+        .attr('transform', 'translate(' + this.marginLines.left + ',' + this.marginLines.top + ')');
+
+      // Call the x axis in a group tag
+      lines.append('g')
+          .attr('class', 'axis')
+          .attr('transform', 'translate(0,' + this.heightLines + ')')
+          .call(d3.axisBottom(this.xScaleLines));
+
+      // Call the y axis in a group tag
+      lines.append('g')
+      .attr('class', 'axis')
+      .call(d3.axisLeft(this.yScaleLines));
+
+      // Generate a line for each line name
+      lineGen = d3.line()
+        .x((d, i) => {return this.xScaleLines(i); }) // set the x values for the line generator
+        .y((d) => { return this.yScaleLines(d); }); // set the y values for the line generator
+
+      // enter the data to the svg
+      this.names.forEach((val, ix) => {
+        lines.append('g')
+        .append('path')
+        .datum(data.map((d) => {return d[val];}))
+        .attr('stroke', this.colors[ix]) // function (d, i) {return c[i];})
+        .attr('class', `line ${val}`)
+        .attr('d', lineGen)
+        .on('click', function(d) {
+          var sel = d3.select(this);
+          console.log(d)
+        console.log(sel.attr('stroke'));
+      });
+      });
+
+    });
+
+
+  this.drawAreas = (data1, data2, c) => {
+
+    // reshape data array from (2, n)
+    // to (n, 2)
+    datas = [data1, data2]
+    var dataLong = [];
+    for (i = 0; i < datas[0].length; i++) {
+      dataLong.push([]);
+    }
+
+    datas.forEach(function (d) {
+      for (var i = 0; i < d.length; i++) {
+        dataLong[i].push([d[i]]);
+      }
+    });
+
+    // Area Generator
+    var area = d3.area()
+    .x((d, i) => { return this.xScaleLines(i);})
+    .y1((d) => { return this.yScaleLines(d[1]);})
+    .y0((d) => { return this.yScaleLines(d[0]);});
+
+    // Draw the area using the reshaped array.
+    var drawarea = lines.append('path')
+    .datum(dataLong)
+    .attr('d', area) // Calls the area generator
+    .attr('class', 'area');
+
+    // Create a linear Gradient to assign different
+    // colors to the areas based on the highest value
+    var linearGradient = svg.append('defs')
+    .append('linearGradient')
+    .attr('id', 'linear-gradient');
+
+    // Compute the intersections of the arrays
+    var intersections = lineIntersection(data1, data2);
+
+    // Transform the intersections values
+    // to percentages to be used as stop points
+    var offScale = d3.scaleLinear()
+    .domain([0, data1.length - 1])
+    .range([0, 100]);
+
+    // Choose which color to start with
+    // based on which curve starts with the
+    // highest value hence is on top off the other.
+    var lastColor = c[0];
+    var nextColor = c[1];
+    var temp;
+    if (data1[0] < data2[0]) {
+      lastColor = c[1];
+      nextColor = c[0];
+    } else if (data1[0] == data2[0]) {
+      if (data1[0] < data2[0]) {
+        lastColor = c[1];
+        nextColor = c[0];
+      }
+    }
+
+    // Always start the linear gradient from
+    // the beginning of the chart
+    linearGradient.append('stop')
+    .attr('offset', '0%')
+    .attr('stop-color', lastColor);
+
+    // Insert two stops per intersection,
+    // the first one where the last color stops,
+    // the second one where the next color begins
+    for (i = 0; i < intersections.length; i++) {
+      var offset = offScale(intersections[i]) + '%';
+
+      linearGradient.append('stop')
+      .attr('offset', offset)
+      .attr('stop-color', lastColor);
+
+      linearGradient.append('stop')
+      .attr('offset', offset)
+      .attr('stop-color', nextColor);
+
+      // siwtch the color to use next after each interception
+      temp = lastColor;
+      lastColor = nextColor;
+      nextColor = temp;
+    }
+
+    linearGradient.append('stop')
+    .attr('offset', '100%')
+    .attr('stop-color', lastColor);
+};
+
+function namesAvg(data) {
+  var avg = [];
+  data.forEach((d,i) => {
+    var row = [];
+    for (var key in d) {
+      if (d.hasOwnProperty(key)) {
+        if (key != 'distance') {
+          row.push(d[key]);
+        }
+      }
+    }
+  avg.push(d3.mean(row));
+  if (Number.isNaN(Math.max(...row))) {
+  console.log(i, row)}
+  });
+  return avg;
+}
+
+function arrayFromJson(data, name) {
+  var arr = [];
+  data.forEach(d => {
+    arr.push(d[name]);
+  });
+  return arr;
+}
+
 // Find the intesection of each segment:
 // pass the first and second array arr1, arr2
 // and the upper index of the array where the
@@ -54,6 +277,8 @@ function lineIntersection(arr1, arr2) {
   return intersections;
 }
 
+// Returns the bar color based on the first array selected
+// depending on the difference of the two arrays
 function barColor(bH, colors) {
   if (bH > 0) {
     return colors[0];
@@ -62,228 +287,77 @@ function barColor(bH, colors) {
   }
 }
 
-function drawSvg(data1, data2) {
-  var svgWidth = 1200;
-  var svgHeight = 750;
-  var svg = d3.select('body')
-  .append('svg')
-  .attr('id', 'main-svg')
-  .attr('width', '100%')
-  .attr('preserveAspectRatio', 'xMidYMid meet')
-  .attr('viewBox', '0 0 ' + svgWidth + ' ' + svgHeight);
 
-  var datas = [data1, data2];
-  var c = ['HotPink', 'gold'];
-  var maxY = d3.max([d3.max(data1), d3.max(data2)]);
-  var chartsDiff = 250;
-  var marginLines = { top: 50, right: 50, bottom: chartsDiff, left: 50 };
-  var widthLines = svgWidth - marginLines.left - marginLines.right;
-  var heightLines = svgHeight - marginLines.top - marginLines.bottom;
-
-  // X Scale
-  var xScaleLines = d3.scaleLinear()
-  .domain([-0.5, data1.length - 0.5])
-  .range([0, widthLines]);
-
-  // Y Scale
-  var yScaleLines = d3.scaleLinear()
-  .domain([0, maxY])
-  .range([heightLines, 0]);
-
-  // Line Generator
-  var line = d3.line()
-  .x(function (d, i) {return xScaleLines(i); }) // set the x values for the line generator
-  .y(function (d) { return yScaleLines(d); }); // set the y values for the line generator
-
-  // SVG dimensions
-  var lines = d3.select('#main-svg')
-    .append('g')
-    .attr('id', 'svg-lines')
-    .attr('transform', 'translate(' + marginLines.left + ',' + marginLines.top + ')');
-
-  // Call the x axis in a group tag
-  lines.append('g')
-      .attr('class', 'axis')
-      .attr('transform', 'translate(0,' + heightLines + ')')
-      .call(d3.axisBottom(xScaleLines).ticks(data1.length));
-
-  // Call the y axis in a group tag
-  lines.append('g')
-  .attr('class', 'axis')
-  .call(d3.axisLeft(yScaleLines));
-
-  // reshape data array from (2, n)
-  // to (n, 2)
-  var dataLong = [];
-  for (i = 0; i < datas[0].length; i++) {
-    dataLong.push([]);
-  }
-
-  datas.forEach(function (d) {
-    for (var i = 0; i < d.length; i++) {
-      dataLong[i].push([d[i]]);
-    }
-  });
-
-  // Area Generator
-  var area = d3.area()
-  .x(function (d, i) { return xScaleLines(i);})
-  .y1(function (d) { return yScaleLines(d[1]);})
-  .y0(function (d) { return yScaleLines(d[0]);});
-
-  // Draw the area using the reshaped array.
-  var drawarea = lines.append('path')
-  .datum(dataLong)
-  .attr('d', area) // Calls the area generator
-  .attr('class', 'area');
-
-  // enter the data to the svg
-  var graph = lines.append('g')
-  .selectAll('graph')
-  .data(datas)
-  .enter()
-  .append('g')
-  .attr('stroke', function (d, i) {return c[i];})
-  .attr('class', 'line');
-
-  // draw the lines
-  graph.append('path')
-  .attr('d', function (d) {return line(d);}); // Calls the line generator
-
-  // Create a linear Gradient to assign different
-  // colors to the areas based on the highest value
-  var linearGradient = svg.append('defs')
-  .append('linearGradient')
-  .attr('id', 'linear-gradient');
-
-  // Compute the intersections of the arrays
-  var intersections = lineIntersection(data1, data2);
-
-  // Transform the intersections values
-  // to percentages to be used as stop points
-  var offScale = d3.scaleLinear()
-  .domain([0, data1.length - 1])
-  .range([0, 100]);
-
-  // Choose which color to start with
-  // based on which curve starts with the
-  // highest value hence is on top off the other.
-  var lastColor = c[0];
-  var nextColor = c[1];
-  var temp;
-  if (data1[0] < data2[0]) {
-    lastColor = c[1];
-    nextColor = c[0];
-  } else if (data1[0] == data2[0]) {
-    if (data1[0] < data2[0]) {
-      lastColor = c[1];
-      nextColor = c[0];
-    }
-  }
-
-  // Always start the linear gradient from
-  // the beginning of the chart
-  linearGradient.append('stop')
-  .attr('offset', '0%')
-  .attr('stop-color', lastColor);
-
-  // Insert two stops per intersection,
-  // the first one where the last color stops,
-  // the second one where the next color begins
-  for (i = 0; i < intersections.length; i++) {
-    var offset = offScale(intersections[i]) + '%';
-
-    linearGradient.append('stop')
-    .attr('offset', offset)
-    .attr('stop-color', lastColor);
-
-    linearGradient.append('stop')
-    .attr('offset', offset)
-    .attr('stop-color', nextColor);
-
-    // siwtch the color to use next after each interception
-    temp = lastColor;
-    lastColor = nextColor;
-    nextColor = temp;
-  }
-
-  linearGradient.append('stop')
-  .attr('offset', '100%')
-  .attr('stop-color', lastColor);
-  var fromTop = svgHeight - chartsDiff + 50;
-  var marginBars = { top: fromTop, right: 50, bottom: 50, left: 50 };
-  var widthBars = svgWidth - marginBars.left - marginBars.right;
-  var heightBars = svgHeight - marginBars.top - marginBars.bottom;
-  var bars = d3.select('svg')
-  .append('g')
-  .attr('id', 'svg-bars')
-  .attr('transform', 'translate(' + marginBars.left + ',' + marginBars.top + ')');
-
-  // Get the bars height
-  var bHeight = [];
-  var bHeightAbs = [];
-  for (var i = 0; i < data1.length; i++) {
-    bHeightAbs.push(Math.abs(data1[i] - data2[i]));
-    bHeight.push(data1[i] - data2[i]);
-  }
-
-  var bands = [];
-  for (i = 0; i < data1.length; i++) {
-    bands.push(i);
-  }
-
-  // X Scale
-  var xScaleBars = d3.scaleBand()
-  .domain(bands)
-  .range([0, widthBars])
-  .padding(0);
-
-  // Compute the stroke-width
-  var strokeWidth = xScaleBars.padding() * xScaleBars.bandwidth();
-
-  // Y Scale
-  var yScaleBars = d3.scaleLinear()
-  .domain([0, d3.max(bHeightAbs)])
-  .range([heightBars, 0]);
-
-  // Call the x axis in a group tag
-  bars.append('g')
-  .attr('class', 'axis')
-  .attr('transform', 'translate(0,' + heightBars + ')')
-  .call(d3.axisBottom(xScaleBars));
-
-  // Call the y axis in a group tag
-  bars.append('g')
-  .attr('class', 'axis')
-  .call(d3.axisLeft(yScaleBars).ticks(3));
-
-  // Draw the bars
-  var bWidth = xScaleBars.bandwidth();
-  bars.selectAll('rect')
-  .data(bHeight)
-  .enter()
-  .append('rect')
-  .attr('width', xScaleBars.bandwidth())
-  .attr('height', function (d) { return heightBars - yScaleBars(Math.abs(d));})
-  .attr('x', function (d, i) { return xScaleBars(i);})
-  .attr('y', function (d) { return yScaleBars(Math.abs(d));})
-  .attr('class', 'bars')
-  .attr('fill', function (d) { return barColor(d, c);})
-  .attr('stroke', function (d) { return barColor(d, c);})
-  .attr('stroke-width', strokeWidth);
+  // var bars = d3.select('svg')
+  // .append('g')
+  // .attr('id', 'svg-bars')
+  // .attr('transform', 'translate(' + this.marginBars.left + ',' + this.marginBars.top + ')');
+  //
+  // // Get the bars height
+  // var bHeight = [];
+  // var bHeightAbs = [];
+  // for (var i = 0; i < data1.length; i++) {
+  //   bHeightAbs.push(Math.abs(data1[i] - data2[i]));
+  //   bHeight.push(data1[i] - data2[i]);
+  // }
+  //
+  // var bands = [];
+  // for (i = 0; i < data1.length; i++) {
+  //   bands.push(i);
+  // }
+  //
+  // // X Scale
+  // var xScaleBars = d3.scaleBand()
+  // .domain(bands)
+  // .range([0, widthBars])
+  // .padding(0);
+  //
+  // // Compute the stroke-width
+  // var strokeWidth = xScaleBars.padding() * xScaleBars.bandwidth();
+  //
+  // // Y Scale
+  // var yScaleBars = d3.scaleLinear()
+  // .domain([0, d3.max(bHeightAbs)])
+  // .range([heightBars, 0]);
+  //
+  // // Call the x axis in a group tag
+  // bars.append('g')
+  // .attr('class', 'axis')
+  // .attr('transform', 'translate(0,' + heightBars + ')')
+  // .call(d3.axisBottom(xScaleBars));
+  //
+  // // Call the y axis in a group tag
+  // bars.append('g')
+  // .attr('class', 'axis')
+  // .call(d3.axisLeft(yScaleBars).ticks(3));
+  //
+  // // Draw the bars
+  // var bWidth = xScaleBars.bandwidth();
+  // bars.selectAll('rect')
+  // .data(bHeight)
+  // .enter()
+  // .append('rect')
+  // .attr('width', xScaleBars.bandwidth())
+  // .attr('height', function (d) { return heightBars - yScaleBars(Math.abs(d));})
+  // .attr('x', function (d, i) { return xScaleBars(i);})
+  // .attr('y', function (d) { return yScaleBars(Math.abs(d));})
+  // .attr('class', 'bars')
+  // .attr('fill', function (d) { return barColor(d, c);})
+  // .attr('stroke', function (d) { return barColor(d, c);})
+  // .attr('stroke-width', strokeWidth);
 }
 
 function main() {
   // generate random arrays
-  var nPoints = Math.round(Math.random() * 25 + 25);
-  var data1 = [];
-  var data2 = [];
-  for (var i = 0; i < nPoints; i++) {
-    data1.push(Math.random() * 10);
-    data2.push(Math.random() * 10);
-  }
-
-  drawSvg(data1, data2);
+  // var nPoints = Math.round(Math.random() * 25 + 25);
+  // var data1 = [];
+  // var data2 = [];
+  // for (var i = 0; i < nPoints; i++) {
+  //   data1.push(Math.random() * 10);
+  //   data2.push(Math.random() * 10);
+  // }
+  var chart = new svgChart();
+  chart.drawCurves()
 }
 
 main();
